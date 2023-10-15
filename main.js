1c8bcb82614c227e8dd717adfeb65071690ad8b9
Kris <kharonek@yahoo.com>
2023-10-15 14:29:16 -0300

    utilised module pattern to create gameState module

--- main.js
+++ main.js
@@ -1,20 +1,42 @@
-const gameBoard = (function() {
-    // 0=' ', 1='x', 2='o'
-    // change name gameBoard to state?
-    const gameBoard = ['', 'x', 'o', '', 'x', '', '', 'o', ''];
-    return { gameBoard };
-}) 
+const gameboard = (function () {
+  // 0=' ', 1='x', 2='o'
+  // change name gameBoard to state?
 
-const renderGameboard = () => {
-    const gameGridElement = document.querySelector('.game-grid');
-    gameGridElement.innerHTML = '';
-    gameBoard().gameBoard.forEach((item) => {
+  // Private memebers
+  const gameboardElement = document.querySelector(".game-grid");
+  const gameboardState = ["", "x", "o", "", "x", "", "", "o", ""];
+
+  function resetGameboard() {
+    gameboardElement.innerHTML = "";
+  }
+
+  // Public members
+  return {
+    renderGameboard: function () {
+      resetGameboard();
+      gameboardState.forEach((item) => {
         const gameItemElement = `<button>${item}</button>`;
         gameGridElement.innerHTML += gameItemElement;
-    })
-}
+      });
+    },
+  };
+})();
 
 // get each of the buttons querySelectorAll
-// 
+//
 
-renderGameboard();
+gameboard.renderGameboard();
+
+const gameState = (function () {
+  let turn = "x";
+
+  return {
+    getTurn: function () {
+      const curTurn = turn;
+      turn = turn === "x" ? "o" : "x";
+      return curTurn;
+    },
+  };
+})(); // add parentheiss to invoke it on creation
+
+console.log(gameState.getTurn());
